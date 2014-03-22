8c2deace74e9c26e5eae5971c0ebbb56efd15b47
Johan Jatko <johan@pie-studios.com>
2014-03-22 22:18:42 +0100

    Time to update it!

--- parse.js
+++ parse.js
@@ -1,16 +1,16 @@
 var request = require('request');
 var fs = require('fs');
-var yaml = require('libyaml');
+var yaml = require('yamljs');
 var languages = {};
 request("https://raw.github.com/github/linguist/master/lib/linguist/languages.yml", function(err, head, body) {
 	var data = yaml.parse(body);
-	console.log(data[0].ASP.color);
-	for(var prop in data[0]) {
-		if("color" in data[0][prop])
+  console.log(data);
+	for(var prop in data) {
+		if("color" in data[prop])
 		{
-			languages[prop] = data[0][prop].color;
+			languages[prop] = data[prop].color;
 		}
 	}
 	json = JSON.stringify(languages);
-	fs.writeFileSync("languages.json", json);
+	fs.writeFileSync("colors.json", json);
 });
